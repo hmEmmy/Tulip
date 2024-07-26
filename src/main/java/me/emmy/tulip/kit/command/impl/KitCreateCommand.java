74b924bac91da8e8fe8a6d6f02b4207d2fdcb35d
Emmy <lisariesey@gmail.com>
2024-07-26 22:33:29 +0200

    feat: new KitCreateCommand & KitDeleteCommand

---
+++ src/main/java/me/emmy/tulip/kit/command/impl/KitCreateCommand.java
@@ -0,0 +1,44 @@
+package me.emmy.tulip.kit.command.impl;
+
+import me.emmy.tulip.Tulip;
+import me.emmy.tulip.kit.Kit;
+import me.emmy.tulip.utils.CC;
+import me.emmy.tulip.utils.command.BaseCommand;
+import me.emmy.tulip.utils.command.CommandArgs;
+import me.emmy.tulip.utils.command.annotation.Command;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+/**
+ * @author Emmy
+ * @project Tulip
+ * @date 26/07/2024 - 22:26
+ */
+public class KitCreateCommand extends BaseCommand {
+    @Override
+    @Command(name = "kit.create", permission = "Tulip.admin")
+    public void onCommand(CommandArgs command) {
+        Player player = command.getPlayer();
+        String[] args = command.getArgs();
+
+        if (args.length < 1) {
+            player.sendMessage(CC.translate("&cUsage: /kit create (name)"));
+            return;
+        }
+
+        String name = args[0];
+
+        if (Tulip.getInstance().getKitRepository().getKit(name) != null) {
+            player.sendMessage(CC.translate("&cA kit with that name already exists."));
+            return;
+        }
+
+        ItemStack[] items = player.getInventory().getContents();
+        ItemStack[] armor = player.getInventory().getArmorContents();
+
+        Kit kit = new Kit(name, "A kit", items, armor, Material.DIAMOND_AXE, 0, true);
+        Tulip.getInstance().getKitRepository().getKits().add(kit);
+        player.sendMessage(CC.translate("&aKit " + name + " has been created."));
+    }
+}
---
+++ src/main/java/me/emmy/tulip/kit/command/impl/KitDeleteCommand.java
@@ -0,0 +1,42 @@
+package me.emmy.tulip.kit.command.impl;
+
+import me.emmy.tulip.Tulip;
+import me.emmy.tulip.kit.Kit;
+import me.emmy.tulip.utils.CC;
+import me.emmy.tulip.utils.command.BaseCommand;
+import me.emmy.tulip.utils.command.CommandArgs;
+import me.emmy.tulip.utils.command.annotation.Command;
+import org.bukkit.entity.Player;
+
+/**
+ * @author Emmy
+ * @project Tulip
+ * @date 26/07/2024 - 22:31
+ */
+public class KitDeleteCommand extends BaseCommand {
+    @Override
+    @Command(name = "kit.delete", permission = "Tulip.admin")
+    public void onCommand(CommandArgs command) {
+        Player player = command.getPlayer();
+        String[] args = command.getArgs();
+
+        if (args.length < 1) {
+            player.sendMessage(CC.translate("&cUsage: /kit delete (name)"));
+            return;
+        }
+
+        String name = args[0];
+
+        if (Tulip.getInstance().getKitRepository().getKit(name) == null) {
+            player.sendMessage(CC.translate("&cA kit with that name does not exist."));
+            return;
+        }
+
+        Kit kit = Tulip.getInstance().getKitRepository().getKit(name);
+        Tulip.getInstance().getKitRepository().deleteKit(kit);
+
+        player.sendMessage(CC.translate("&aKit " + name + " has been deleted."));
+
+
+    }
+}
