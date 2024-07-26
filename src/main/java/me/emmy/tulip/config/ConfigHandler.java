4bb852eea906db71b50df83aca926b4f16bd56f6
Emmy <lisariesey@gmail.com>
2024-07-26 22:19:57 +0200

    fix: ConfigHandler was being ignored by git

---
+++ src/main/java/me/emmy/tulip/config/ConfigHandler.java
@@ -0,0 +1,93 @@
+package me.emmy.tulip.config;
+
+import lombok.Getter;
+import me.emmy.tulip.Tulip;
+import me.emmy.tulip.utils.Logger;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Emmy
+ * @project Tulip
+ * @date 26/07/2024 - 21:28
+ */
+@Getter
+public class ConfigHandler {
+
+    @Getter private static ConfigHandler instance;
+
+    private final Tulip plugin = Tulip.getInstance();
+
+    private final Map<String, File> configFiles = new HashMap<>();
+    private final Map<String, FileConfiguration> fileConfigurations = new HashMap<>();
+
+    private final FileConfiguration settingsConfig;
+    private final FileConfiguration localeConfig;
+    private final FileConfiguration arenasConfig;
+    private final FileConfiguration kitsConfig;
+
+    private final String[] configFileNames = {
+            "settings.yml", "locale.yml", "storage/arenas.yml"
+    };
+
+    public ConfigHandler() {
+        for (String fileName : configFileNames) {
+            loadConfig(fileName);
+        }
+
+        instance = this;
+
+        settingsConfig = getConfig("settings.yml");
+        localeConfig = getConfig("locale.yml");
+        arenasConfig = getConfig("storage/arenas.yml");
+        kitsConfig = getConfig("storage/kits.yml");
+    }
+
+    private void loadConfig(String fileName) {
+        File configFile = new File(plugin.getDataFolder(), fileName);
+        configFiles.put(fileName, configFile);
+        if (!configFile.exists()) {
+            configFile.getParentFile().mkdirs();
+            plugin.saveResource(fileName, false);
+        }
+
+        FileConfiguration config = YamlConfiguration.loadConfiguration(configFile);
+        fileConfigurations.put(fileName, config);
+    }
+
+    public void reloadConfigs() {
+        for (String fileName : configFileNames) {
+            loadConfig(fileName);
+        }
+    }
+
+    public void saveConfigs() {
+        for (Map.Entry<String, FileConfiguration> entry : fileConfigurations.entrySet()) {
+            String fileName = entry.getKey();
+            FileConfiguration config = entry.getValue();
+            File configFile = configFiles.get(fileName);
+            saveConfig(configFile, config);
+        }
+    }
+
+    public void saveConfig(File configFile, FileConfiguration fileConfiguration) {
+        try {
+            fileConfiguration.save(configFile);
+            fileConfiguration.load(configFile);
+        } catch (Exception e) {
+            Logger.logError("Failed to save config file: " + configFile.getName());
+        }
+    }
+
+    public FileConfiguration getConfig(String fileName) {
+        return fileConfigurations.get(fileName);
+    }
+
+    public File getConfigFile(String fileName) {
+        return configFiles.get(fileName);
+    }
+}
