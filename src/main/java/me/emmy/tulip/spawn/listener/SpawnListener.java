c628625bc8852c47e8325b78c26add23b4d70650
Emmy <lisariesey@gmail.com>
2024-08-10 16:29:56 +0200

    feat: cancel damage if profile state equals "SPAWN"

--- src/main/java/me/emmy/tulip/spawn/listener/SpawnListener.java
+++ src/main/java/me/emmy/tulip/spawn/listener/SpawnListener.java
@@ -1,13 +1,17 @@
-package me.emmy.tulip.essential.listener;
+package me.emmy.tulip.spawn.listener;
 
 import me.emmy.tulip.Tulip;
 import me.emmy.tulip.profile.enums.EnumProfileState;
 import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.EntityDamageByBlockEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.FoodLevelChangeEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.inventory.InventoryType;
@@ -20,7 +24,7 @@ import org.bukkit.event.weather.WeatherChangeEvent;
  * @project Tulip
  * @date 29/07/2024 - 00:11
  */
-public class WorldListener implements Listener {
+public class SpawnListener implements Listener {
 
     @EventHandler
     private void onWeatherChange(WeatherChangeEvent event) {
@@ -91,6 +95,36 @@ public class WorldListener implements Listener {
 
         if (Tulip.getInstance().getProfileRepository().getProfile(player.getUniqueId()).getState() == EnumProfileState.SPAWN) {
             if (player.getGameMode() == GameMode.SURVIVAL) {
+                event.setCancelled(true);
+            }
+        }
+    }
+
+    @EventHandler
+    private void onEntityDamageByEntity(EntityDamageByEntityEvent event) {
+        if (event.getEntity() instanceof Player) {
+            Player victim = (Player) event.getEntity();
+            if (Tulip.getInstance().getProfileRepository().getProfile(victim.getUniqueId()).getState() == EnumProfileState.SPAWN) {
+                event.setCancelled(true);
+            }
+        }
+    }
+
+    @EventHandler
+    private void onEntityDamage(EntityDamageEvent event) {
+        if (event.getEntity() instanceof Player) {
+            Player player = (Player) event.getEntity();
+            if (Tulip.getInstance().getProfileRepository().getProfile(player.getUniqueId()).getState() == EnumProfileState.SPAWN) {
+                event.setCancelled(true);
+            }
+        }
+    }
+
+    @EventHandler
+    private void onEntityBlockDamage(EntityDamageByBlockEvent event) {
+        if (event.getEntity() instanceof Player) {
+            Player player = (Player) event.getEntity();
+            if (Tulip.getInstance().getProfileRepository().getProfile(player.getUniqueId()).getState() == EnumProfileState.SPAWN) {
                 event.setCancelled(true);
             }
         }
